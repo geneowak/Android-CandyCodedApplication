d8fb4120c6f2960496b0b599d82a24820d61c963
geneowak <teugeneowak@gmail.com>
2019-11-01 16:40:15 +0300

    Start the Google Maps Activity

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -87,9 +87,6 @@ public class MainActivity extends AppCompatActivity {
         startActivity(candyInfo);
         return super.onOptionsItemSelected(item);
     }
-    // ***
-    // TODO - Task 1 - Show Store Information Activity
-    // ***
 
     private void addCandiesToDatabase(Candy[] candies) {
         SQLiteDatabase db = candyDbHelper.getWritableDatabase();
